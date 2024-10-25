dbfe5fce7b5cf4ef30486a0798b1e7ad7dc9b338
mgalihpp <muhammadgalih451@gmail.com>
2024-10-25 09:28:20 +0700

    Set up migrations table

--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -1,11 +1,11 @@
-import { createInertiaApp } from '@inertiajs/react';
-import createServer from '@inertiajs/react/server';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import ReactDOMServer from 'react-dom/server';
-import { RouteName } from 'ziggy-js';
-import { route } from '../../vendor/tightenco/ziggy';
+import { createInertiaApp } from "@inertiajs/react";
+import createServer from "@inertiajs/react/server";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import ReactDOMServer from "react-dom/server";
+import { RouteName } from "ziggy-js";
+import { route } from "../../vendor/tightenco/ziggy";
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 createServer((page) =>
     createInertiaApp({
@@ -14,22 +14,20 @@ createServer((page) =>
         title: (title) => `${title} - ${appName}`,
         resolve: (name) =>
             resolvePageComponent(
-                `./Pages/${name}.tsx`,
-                import.meta.glob('./Pages/**/*.tsx'),
+                `./pages/${name}.tsx`,
+                import.meta.glob("./pages/**/*.tsx")
             ),
         setup: ({ App, props }) => {
             /* eslint-disable */
             // @ts-expect-error
             global.route<RouteName> = (name, params, absolute) =>
                 route(name, params as any, absolute, {
-                    // @ts-expect-error
                     ...page.props.ziggy,
-                    // @ts-expect-error
                     location: new URL(page.props.ziggy.location),
                 });
             /* eslint-enable */
 
             return <App {...props} />;
         },
-    }),
+    })
 );
