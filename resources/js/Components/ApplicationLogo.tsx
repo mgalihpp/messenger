2dae67d6f9f2a1ddcafb6b87cd76ee82f0e464c1
mgalihpp <68680851+mgalihpp@users.noreply.github.com>
2024-11-02 14:18:48 +0700

    Delete resources/js/Components directory

--- resources/js/Components/ApplicationLogo.tsx
+++
@@ -1,7 +0,0 @@
-import { ImgHTMLAttributes } from "react";
-
-export default function ApplicationLogo(
-  props: ImgHTMLAttributes<HTMLImageElement>,
-) {
-  return <img src="/images/logo.png" alt="" {...props} />;
-}
