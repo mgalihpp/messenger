94848f11ed77a56015e17a27944471e311edd42a
mgalihpp <muhammadgalih451@gmail.com>
2024-11-02 14:17:36 +0700

    Update Folder case

---
+++ resources/js/pages/auth/Register.tsx
@@ -0,0 +1,134 @@
+import InputError from "@/components/InputError";
+import InputLabel from "@/components/InputLabel";
+import PrimaryButton from "@/components/PrimaryButton";
+import TextInput from "@/components/TextInput";
+import { Input } from "@/components/ui/input";
+import GuestLayout from "@/layouts/GuestLayout";
+import { RegisterUserSchema } from "@/types/user";
+import { Head, Link, useForm } from "@inertiajs/react";
+import { Eye, EyeClosed } from "lucide-react";
+import { FormEventHandler, useState } from "react";
+
+export default function Register() {
+  const { data, setData, post, processing, errors, reset } =
+    useForm<RegisterUserSchema>({
+      name: "",
+      email: "",
+      password: "",
+      password_confirmation: "",
+    });
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const submit: FormEventHandler = (e) => {
+    e.preventDefault();
+
+    post(route("register"), {
+      onFinish: () => reset("password", "password_confirmation"),
+    });
+  };
+
+  return (
+    <GuestLayout>
+      <Head title="Register" />
+
+      <form onSubmit={submit} className="space-y-4">
+        <div>
+          <InputLabel htmlFor="name" value="Name" />
+
+          <Input
+            variant="form_login"
+            id="name"
+            name="name"
+            value={data.name}
+            className="mt-1 block w-full"
+            autoComplete="name"
+            autoFocus
+            onChange={(e) => setData("name", e.target.value)}
+          />
+
+          <InputError message={errors.name} className="mt-2" />
+        </div>
+
+        <div>
+          <InputLabel htmlFor="email" value="Email" />
+
+          <Input
+            variant="form_login"
+            id="email"
+            type="email"
+            name="email"
+            value={data.email}
+            className="mt-1 block w-full"
+            autoComplete="username"
+            onChange={(e) => setData("email", e.target.value)}
+          />
+
+          <InputError message={errors.email} className="mt-2" />
+        </div>
+
+        <div>
+          <InputLabel htmlFor="password" value="Password" />
+
+          <div className="relative">
+            <Input
+              variant="form_login"
+              id="password"
+              type={showPassword ? "text" : "password"}
+              name="password"
+              value={data.password}
+              className="mt-1 block w-full"
+              autoComplete="new-password"
+              onChange={(e) => setData("password", e.target.value)}
+            />
+            <InputError message={errors.password} className="mt-2" />
+
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute right-2.5 top-3.5"
+            >
+              {showPassword ? (
+                <Eye className="size-4" />
+              ) : (
+                <EyeClosed className="size-4" />
+              )}
+            </button>
+          </div>
+        </div>
+
+        <div>
+          <InputLabel
+            htmlFor="password_confirmation"
+            value="Confirm Password"
+          />
+
+          <Input
+            variant="form_login"
+            id="password_confirmation"
+            type="password"
+            name="password_confirmation"
+            value={data.password_confirmation}
+            className="mt-1 block w-full"
+            autoComplete="new-password"
+            onChange={(e) => setData("password_confirmation", e.target.value)}
+          />
+
+          <InputError message={errors.password_confirmation} className="mt-2" />
+        </div>
+
+        <div className="flex">
+          <PrimaryButton className="w-full" disabled={processing}>
+            Register
+          </PrimaryButton>
+        </div>
+
+        <div className="flex justify-center">
+          <Link href={route("login")} className="btn btn-link">
+            Already registered?
+          </Link>
+        </div>
+      </form>
+    </GuestLayout>
+  );
+}
---
+++ resources/js/pages/welcome/Index.tsx
@@ -0,0 +1,143 @@
+import ApplicationLogo from "@/components/ApplicationLogo";
+import InputError from "@/components/InputError";
+import PrimaryButton from "@/components/PrimaryButton";
+import { LoginSchema } from "@/types/user";
+import { Input } from "@/components/ui/input";
+import { Link, useForm } from "@inertiajs/react";
+import { Eye, EyeClosed } from "lucide-react";
+import { useState, FormEventHandler } from "react";
+
+export default function Welcome({
+  canResetPassword,
+  appName,
+}: {
+  canResetPassword: boolean;
+  appName: string;
+}) {
+  const { data, setData, post, processing, errors, reset } =
+    useForm<LoginSchema>({
+      email: "",
+      password: "",
+      remember: false,
+    });
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const submit: FormEventHandler = (e) => {
+    e.preventDefault();
+
+    post(route("login"), {
+      onFinish: () => reset("password"),
+    });
+  };
+
+  return (
+    <section className="mx-auto flex min-h-screen max-w-7xl flex-col gap-12 p-8 font-['Inter'] text-foreground">
+      <div>
+        <Link href="/">
+          <ApplicationLogo className="h-10" />
+        </Link>
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2">
+        <div className="space-y-6 sm:w-11/12 sm:space-y-12">
+          <h1 className="text-4xl font-bold sm:text-5xl lg:text-7xl">
+            <span className="bg-gradient-to-r from-blue-600 via-purple-500 to-rose-500 bg-clip-text text-transparent">
+              Hang out
+            </span>
+            <br />
+            <span className="bg-gradient-to-r from-blue-600 via-purple-500 to-rose-500 bg-clip-text text-transparent">
+              anytime,
+            </span>
+            <span className="bg-gradient-to-r from-blue-600 via-purple-500 to-rose-500 bg-clip-text text-transparent">
+              &nbsp; anywhere
+            </span>
+          </h1>
+
+          <p className="text-lg sm:text-xl">
+            Messenger makes it easy and fun to stay close to your favorite
+            people.
+          </p>
+
+          <form onSubmit={submit} className="flex flex-col gap-4 lg:w-3/4">
+            <div>
+              <Input
+                variant="form_login"
+                id="email"
+                type="email"
+                name="email"
+                value={data.email}
+                className="mt-1 block w-full bg-muted"
+                autoComplete="username"
+                autoFocus
+                placeholder="Enter your email address"
+                onChange={(e) => setData("email", e.target.value)}
+              />
+
+              <InputError message={errors.email} className="mt-2" />
+            </div>
+
+            <div>
+              <div className="relative">
+                <Input
+                  variant="form_login"
+                  id="password"
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  value={data.password}
+                  className="mt-1 block w-full bg-muted"
+                  autoComplete="current-password"
+                  placeholder="Enter your password"
+                  onChange={(e) => setData("password", e.target.value)}
+                />
+                <InputError message={errors.password} className="mt-2" />
+
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute right-2.5 top-3.5"
+                >
+                  {showPassword ? (
+                    <Eye className="size-4" />
+                  ) : (
+                    <EyeClosed className="size-4" />
+                  )}
+                </button>
+              </div>
+            </div>
+
+            <div className="flex items-center justify-end">
+              {canResetPassword && (
+                <Link href={route("password.request")} className="btn btn-link">
+                  Forgot your password?
+                </Link>
+              )}
+            </div>
+
+            <div className="flex">
+              <PrimaryButton className="w-full" disabled={processing}>
+                Log in
+              </PrimaryButton>
+            </div>
+
+            <div className="flex justify-center">
+              <Link href={route("register")} className="btn btn-link">
+                Don't have an account?
+              </Link>
+            </div>
+          </form>
+        </div>
+
+        <div className="hidden items-center justify-center sm:flex">
+          <img src="/images/vector.png" alt="vector" />
+        </div>
+      </div>
+
+      <div className="mt-auto flex gap-2 sm:justify-center">
+        <span className="bg-gradient-to-r from-blue-600 via-purple-500 to-rose-500 bg-clip-text text-sm font-medium text-transparent">
+          &copy; {appName} {new Date().getFullYear()}
+        </span>
+      </div>
+    </section>
+  );
+}
